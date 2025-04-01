381c0806c8a256a16ea1cb124bed79f2b14e00a0
ivan200126y <ivan29654@gmail.com>
2025-04-01 14:41:39 +0300

    Initial Commit

---
+++ src/main/java/com/telerikacademy/web/cryptocurrency_trading_platform/CryptocurrencyTradingPlatformApplication.java
@@ -0,0 +1,15 @@
+package com.telerikacademy.web.cryptocurrency_trading_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptocurrencyTradingPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptocurrencyTradingPlatformApplication.class, args);
+    }
+
+    //db - transactions, user
+
+}
---
+++ src/test/java/com/telerikacademy/web/cryptocurrency_trading_platform/CryptocurrencyTradingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telerikacademy.web.cryptocurrency_trading_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptocurrencyTradingPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
